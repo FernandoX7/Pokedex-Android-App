41a9f9afdaade2426e883e00dc3e52a070323f24
Fernando Ramirez <framirez1997@gmail.com>
2021-04-22 08:12:31 -0500

    Add PokemonRepositoryImpl

---
+++ app/src/main/java/com/fernando/ramirez/pokedex/data/repository/pokemon/PokemonRepositoryImpl.kt
@@ -0,0 +1,87 @@
+package com.fernando.ramirez.pokedex.data.repository.pokemon
+
+import com.fernando.ramirez.pokedex.data.domain.repository.PokemonRepository
+import com.fernando.ramirez.pokedex.data.io.db.pokemon.Pokemon
+import com.fernando.ramirez.pokedex.data.repository.pokemon.datasource.PokemonCacheDataSource
+import com.fernando.ramirez.pokedex.data.repository.pokemon.datasource.PokemonDbDataSource
+import com.fernando.ramirez.pokedex.data.repository.pokemon.datasource.PokemonRemoteDataSource
+import timber.log.Timber
+
+class PokemonRepositoryImpl(
+  private val pokemonCacheDataSource: PokemonCacheDataSource,
+  private val pokemonDbDataSource: PokemonDbDataSource,
+  private val pokemonRemoteDataSource: PokemonRemoteDataSource,
+) : PokemonRepository {
+
+  override suspend fun deleteAll(): Int {
+    return pokemonDbDataSource.deleteAll()
+  }
+
+  override suspend fun getAll(): List<Pokemon> {
+    return getPokemonFromCache()
+  }
+
+  override suspend fun updateAll(): List<Pokemon> {
+    val newListOfPokemon = getPokemonFromApi()
+
+    pokemonDbDataSource.deleteAll()
+
+    pokemonDbDataSource.savePokemonToDb(newListOfPokemon)
+    pokemonCacheDataSource.savePokemonToCache(newListOfPokemon)
+
+    return newListOfPokemon
+  }
+
+  private suspend fun getPokemonFromCache(): List<Pokemon> {
+    var pokemonList: List<Pokemon> = listOf()
+
+    try {
+      pokemonList = pokemonCacheDataSource.getPokemonFromCache()
+    } catch (e: Exception) {
+      Timber.e(e)
+    }
+
+    if (pokemonList.isNotEmpty()) {
+      return pokemonList
+    } else {
+      pokemonList = getPokemonFromDb()
+      pokemonCacheDataSource.savePokemonToCache(pokemonList)
+    }
+    return pokemonList
+  }
+
+  private suspend fun getPokemonFromDb(): List<Pokemon> {
+    var pokemonList: List<Pokemon> = listOf()
+
+    try {
+      pokemonList = pokemonDbDataSource.getPokemonFromDb()
+    } catch (e: Exception) {
+      Timber.e(e)
+    }
+
+    if (pokemonList.isNotEmpty()) {
+      return pokemonList
+    } else {
+      pokemonList = getPokemonFromApi()
+      pokemonDbDataSource.savePokemonToDb(pokemonList)
+    }
+
+    return pokemonList
+  }
+
+  private suspend fun getPokemonFromApi(): List<Pokemon> {
+    var pokemonList: List<Pokemon> = listOf()
+
+    try {
+      val response = pokemonRemoteDataSource.getPokemon()
+      val body = response.body()
+      if (body != null) {
+        pokemonList = body.results
+      }
+    } catch (e: Exception) {
+      Timber.e(e)
+    }
+
+    return pokemonList
+  }
+}
