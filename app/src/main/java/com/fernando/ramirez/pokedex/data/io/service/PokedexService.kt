48202456f225270014022688610855410c907be5
Fernando Ramirez <framirez1997@gmail.com>
2021-04-22 08:05:59 -0500

    Add getPokemon() service function

--- app/src/main/java/com/fernando/ramirez/pokedex/data/io/service/PokedexService.kt
+++ app/src/main/java/com/fernando/ramirez/pokedex/data/io/service/PokedexService.kt
@@ -1,4 +1,10 @@
 package com.fernando.ramirez.pokedex.data.io.service
 
+import com.fernando.ramirez.pokedex.data.io.response.PokemonResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface PokedexService {
+  @GET("/api/v2/pokemon")
+  suspend fun getPokemon(): Response<PokemonResponse>
 }
