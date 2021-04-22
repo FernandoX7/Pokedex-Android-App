544d6bfc3eeff48ec4ff190c9730638410561814
Fernando Ramirez <framirez1997@gmail.com>
2021-04-22 08:07:22 -0500

    Inject GetPokemonUseCase into PokedexViewModelFactory

--- app/src/main/java/com/fernando/ramirez/pokedex/ui/presentation/PokedexViewModelFactory.kt
+++ app/src/main/java/com/fernando/ramirez/pokedex/ui/presentation/PokedexViewModelFactory.kt
@@ -2,12 +2,17 @@ package com.fernando.ramirez.pokedex.ui.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.fernando.ramirez.pokedex.data.domain.usecase.GetPokemonUseCase
 
-class PokedexViewModelFactory() : ViewModelProvider.Factory {
+class PokedexViewModelFactory(
+  private val getPokemonUseCase: GetPokemonUseCase,
+) : ViewModelProvider.Factory {
 
   override fun <T : ViewModel?> create(modelClass: Class<T>): T {
     if (modelClass.isAssignableFrom(PokedexViewModel::class.java)) {
-      return PokedexViewModel() as T
+      return PokedexViewModel(
+        getPokemonUseCase
+      ) as T
     }
     throw IllegalArgumentException("Unknown View Model")
   }
