631a94fc9aa996b804cea36d8b8fa5830313605b
Fernando Ramirez <framirez1997@gmail.com>
2021-04-22 07:54:41 -0500

    Add PokemonRepository

---
+++ app/src/main/java/com/fernando/ramirez/pokedex/data/domain/repository/PokemonRepository.kt
@@ -0,0 +1,9 @@
+package com.fernando.ramirez.pokedex.data.domain.repository
+
+import com.fernando.ramirez.pokedex.data.io.db.pokemon.Pokemon
+
+interface PokemonRepository {
+  suspend fun deleteAll(): Int
+  suspend fun getAll(): List<Pokemon>
+  suspend fun updateAll(): List<Pokemon>
+}
