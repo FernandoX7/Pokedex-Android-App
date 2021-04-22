339da67dfba929b8324fdbf79d8974bfb15ddc03
Fernando Ramirez <framirez1997@gmail.com>
2021-04-22 07:55:26 -0500

    Add GetPokemonUseCase

---
+++ app/src/main/java/com/fernando/ramirez/pokedex/data/domain/usecase/GetPokemonUseCase.kt
@@ -0,0 +1,13 @@
+package com.fernando.ramirez.pokedex.data.domain.usecase
+
+import com.fernando.ramirez.pokedex.data.domain.repository.PokemonRepository
+import com.fernando.ramirez.pokedex.data.io.db.pokemon.Pokemon
+
+class GetPokemonUseCase(
+  private val pokemonRepository: PokemonRepository,
+) {
+
+  suspend fun execute(): List<Pokemon> {
+    return pokemonRepository.getAll()
+  }
+}
