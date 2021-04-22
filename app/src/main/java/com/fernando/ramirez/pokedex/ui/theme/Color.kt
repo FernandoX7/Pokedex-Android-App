ea3729b81ba95588dc1c1a2003c6b0f4cc150b05
Fernando Ramirez <framirez1997@gmail.com>
2021-04-22 11:47:00 -0500

    Change default colors

--- app/src/main/java/com/fernando/ramirez/pokedex/ui/theme/Color.kt
+++ app/src/main/java/com/fernando/ramirez/pokedex/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.fernando.ramirez.pokedex.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val Red200 = Color(0xFFFC2933)
+val Red500 = Color(0xFF7C0B11)
+val Red700 = Color(0xFF610106)
 val Teal200 = Color(0xFF03DAC5)
