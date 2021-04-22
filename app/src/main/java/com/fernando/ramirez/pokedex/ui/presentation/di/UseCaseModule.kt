4e419b629e994c203db2599c280518c352e9bd3e
Fernando Ramirez <framirez1997@gmail.com>
2021-04-22 08:06:47 -0500

    Add UseCaseModule

---
+++ app/src/main/java/com/fernando/ramirez/pokedex/ui/presentation/di/UseCaseModule.kt
@@ -0,0 +1,20 @@
+package com.fernando.ramirez.pokedex.ui.presentation.di
+
+import com.fernando.ramirez.pokedex.data.domain.repository.PokemonRepository
+import com.fernando.ramirez.pokedex.data.domain.usecase.GetPokemonUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class UseCaseModule {
+
+  @Provides
+  @Singleton
+  fun providesGetPokemonUseCase(
+    pokemonRepository: PokemonRepository,
+  ): GetPokemonUseCase = GetPokemonUseCase(pokemonRepository)
+}
