c2014e0318ce8593c84abd24d1ea4af205273a42
Fernando Ramirez <framirez1997@gmail.com>
2021-04-21 19:27:48 -0500

    Add Retrofit library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,10 @@ dependencies {
   implementation("com.squareup.okhttp3:okhttp:4.9.0")
   implementation("com.squareup.okhttp3:logging-interceptor:4.7.2")
 
+  // Retrofit
+  implementation("com.squareup.retrofit2:retrofit:2.9.0")
+  implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
   testImplementation("junit:junit:4.13.2")
 
   androidTestImplementation("androidx.test.ext:junit:1.1.2")
