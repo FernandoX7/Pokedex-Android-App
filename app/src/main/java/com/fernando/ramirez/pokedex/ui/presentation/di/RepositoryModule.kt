1e5e82ca0a30e9e020873579777c12b644250c16
Fernando Ramirez <framirez1997@gmail.com>
2021-04-22 08:12:40 -0500

    Add RepositoryModule

---
+++ app/src/main/java/com/fernando/ramirez/pokedex/ui/presentation/di/RepositoryModule.kt
@@ -0,0 +1,31 @@
+package com.fernando.ramirez.pokedex.ui.presentation.di
+
+import com.fernando.ramirez.pokedex.data.domain.repository.PokemonRepository
+import com.fernando.ramirez.pokedex.data.repository.pokemon.PokemonRepositoryImpl
+import com.fernando.ramirez.pokedex.data.repository.pokemon.datasource.PokemonCacheDataSource
+import com.fernando.ramirez.pokedex.data.repository.pokemon.datasource.PokemonDbDataSource
+import com.fernando.ramirez.pokedex.data.repository.pokemon.datasource.PokemonRemoteDataSource
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RepositoryModule {
+
+  @Provides
+  @Singleton
+  fun providesPokemonRepository(
+    pokemonCacheDataSource: PokemonCacheDataSource,
+    pokemonDbDataSource: PokemonDbDataSource,
+    pokemonRemoteDataSource: PokemonRemoteDataSource,
+  ): PokemonRepository {
+    return PokemonRepositoryImpl(
+      pokemonCacheDataSource,
+      pokemonDbDataSource,
+      pokemonRemoteDataSource,
+    )
+  }
+}
